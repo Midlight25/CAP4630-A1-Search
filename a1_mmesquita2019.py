bd4c3a377efc813445839af1c6c733dc6dc627a7
Michael Mesquita <mannolaur@gmail.com>
2021-06-06 14:00:47 -0400

    Added DFS function

--- a1_mmesquita2019.py
+++ a1_mmesquita2019.py
@@ -91,6 +91,63 @@ def breadth_first_search(
     return None
 
 
+def depth_first_search(
+    start: str,
+    goal: str,
+    graph: DefaultDict[str, List[Tuple[str, int]]]
+) -> Union[CityNode, None]:
+    """
+        Depth First Search
+        Pre-Condition: The start and goal parameters are strings that contain
+            valid city names found in DefaultDict
+        Post-Condition: A reverse link-list node is returned with a link
+            to the node that was used to visit that city. If no path
+            between start and goal is found, nothing is returned.
+        Description: A path between start and goal is attempted to be found
+            using a stack. If a path is found, a node network based on
+            what path was taken to reach that node is used to identify
+            a list of parents from goal to start.
+    """
+    # This list will contain the names of all nodes that have already been
+    # visited by the function.
+    visited: List[str] = []
+
+    # This double-ended stack is used to hold all new nodes found during the
+    # load-branches phase of the algorithm.
+    stack: Deque[CityNode] = deque()
+
+    # Create the first node for the starting city and load it into the
+    # stack.
+    stack.append(CityNode(start))
+
+    # As soon as we run out of items in the stack, this loop ends.
+    while stack:
+
+        current_node: CityNode = stack.pop()
+        visited.append(current_node.name)
+
+        # If this city is our goal, then we return this node with all it's
+        # parents
+        if current_node.name == goal:
+            return current_node
+
+        # If the current city is not our goal, then we look at it's neighbors
+        # Remember that every destination is a tuple. [name, cost]
+        for destination in graph[current_node.name]:
+
+            # Prevent cycles by checking the name of the destination
+            # against the names in visited.
+            if destination[0] not in visited:
+
+                # Add new destinations to the stack for processing
+                # later.
+                stack.append(CityNode(destination[0], current_node))
+
+    # If we can't find a path from start to goal, then we return
+    # none.
+    return None
+
+
 if __name__ == "__main__":
 
     # This is the encoding of all the paths between the cities. The format is:
